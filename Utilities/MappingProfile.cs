d6e6399c5fdaf5c5ab1cbf392524d13daf8d39cb
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-05-25 00:29:53 +0200

    Added automapper

---
+++ Utilities/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BattAnimeZone.Components.Models.Anime;
+using BattAnimeZone.Components.Models.AnimeDTOs;
+
+namespace BattAnimeZone.Utilities
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Anime, AnimeHomePageDTO>();
+		}
+
+	}
+}
